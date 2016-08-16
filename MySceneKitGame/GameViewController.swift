b9d2e25e0f757e8510838a9c8178e10ee99a0708
Connor Mosley <connor.l.mosley@gmail.com>
2016-08-16 00:50:43 -0700

    Added a gun that now moves with the user's finger and the bullets emit from the tip of the gun in the direction that the gun is pointing.

--- MySceneKitGame/GameViewController.swift
+++ MySceneKitGame/GameViewController.swift
@@ -16,12 +16,14 @@ class GameViewController: UIViewController {
     var cameraNode: SCNNode!
     var spawnTime:NSTimeInterval = 0
     var game = GameHelper.sharedInstance
+    var gun : SCNNode!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         setupScene()
         setupCamera()
+        gun = makeGun()
         setupHUD()
     }
     
@@ -52,66 +54,118 @@ class GameViewController: UIViewController {
     func setupCamera() {
         cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
-        cameraNode.position = SCNVector3(x: 0, y: 5, z: 20)
+        cameraNode.position = SCNVector3(x: 0, y: 5, z: 17)
+        cameraNode.camera?.zFar = 200.0
         scnScene.rootNode.addChildNode(cameraNode)
     }
     
-    func spawnShape() {
+    func spawnDisks() {
         var geometry : SCNGeometry
-        switch ShapeType.random() {
-            case .Sphere:
-                geometry = SCNSphere(radius: 1.0)
-            case .Pyramid:
-                geometry = SCNPyramid(width: 1.0, height: 1.0, length: 1.0)
-            case .Torus:
-                geometry = SCNTorus(ringRadius: 1.0, pipeRadius: 0.5)
-            case .Capsule:
-                geometry = SCNCapsule(capRadius: 1.0, height: 1.5)
-            case .Cylinder:
-                geometry = SCNCylinder(radius: 1.0, height: 1.5)
-            case .Cone:
-                geometry = SCNCone(topRadius: 0.0, bottomRadius: 1.0, height: 1.0)
-            case .Tube:
-                geometry = SCNTube(innerRadius: 0.5, outerRadius: 1.0, height: 1.0)
-            default:
-                geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
-        }
+        geometry = SCNCone(topRadius: 1.5, bottomRadius: 2.0, height: 0.7)
+        let physicsShape = SCNPhysicsShape(geometry: SCNTorus(ringRadius: 2.0, pipeRadius: 1.0), options: nil)
         let geometryNode = SCNNode(geometry: geometry)
         //nil for the physics shape will automatically generate a shape based on the geometry of the node
-        geometryNode.physicsBody = SCNPhysicsBody(type: .Dynamic, shape: nil)
-        let randomX = Float.random(min: -2, max: 2)
-        let randomY = Float.random(min: 10, max: 18)
-        let force = SCNVector3(x: randomX, y: randomY , z: 0)
-        let position = SCNVector3(x: 0.05, y: 0.05, z: 0.05)
-        geometryNode.physicsBody?.applyForce(force, atPosition: position, impulse: true)
-        let color = UIColor.random()
-        geometry.materials.first?.diffuse.contents = color
-        let trailEmitter = createTrail(color, geometry: geometry)
-        geometryNode.addParticleSystem(trailEmitter)
-        if color == UIColor.blackColor() {
-            geometryNode.name = "BAD"
+        geometryNode.physicsBody = SCNPhysicsBody(type: .Dynamic, shape: physicsShape)
+        let randomX = Float.random(min: 1, max: 10)
+        let randomY = Float.random(min: 18, max: 30)
+        let randomZ = Float.random(min: 13, max: 30)
+        let randomSide = Int(arc4random_uniform(2))
+        var force : SCNVector3
+        if randomSide == 1 {
+            geometryNode.position.x = -8
+            force = SCNVector3(x: randomX, y: randomY , z: -randomZ)
         } else {
-            geometryNode.name = "GOOD"
+            geometryNode.position.x = 8
+            force = SCNVector3(x: -randomX, y: randomY , z: -randomZ)
         }
+        geometryNode.physicsBody?.applyForce(force, impulse: true)
+        geometry.materials.first?.diffuse.contents = UIColor.orangeColor()
+        geometryNode.name = "Disk"
         scnScene.rootNode.addChildNode(geometryNode)
     }
     
-    func fireGun(direction: SCNVector3) {
-        let geometryNode = SCNNode(geometry: SCNSphere(radius: 0.1))
-        geometryNode.physicsBody = SCNPhysicsBody(type: .Dynamic, shape: nil)
-        geometryNode.physicsBody?.affectedByGravity = false
+    func fireGun(xTouch : CGFloat, yTouch : CGFloat) {
+//        let xDir = -(self.scnView.center.x - xTouch)/3
+//        let yDir = (self.scnView.frame.midY - yTouch)/3
+//        let direction = SCNVector3(xDir, yDir, -150)
+        let direction = SCNVector3(0, 0, -150)
+        let geometryNode = makeBullet()
         geometryNode.physicsBody?.applyForce(direction, impulse: true)
-        geometryNode.geometry?.materials.first?.diffuse.contents = UIColor.blackColor()
-        geometryNode.position.z = 18
-        geometryNode.name = "bullet"
-        geometryNode.physicsBody?.contactTestBitMask = 1
-        scnScene.rootNode.addChildNode(geometryNode)
+        geometryNode.position.z = -2
+        geometryNode.position.y = 1
+        gun.addChildNode(geometryNode)
+    }
+    
+    func makeGun() -> SCNNode {
+        let gunStockShape = SCNBox(width: 1.0, height: 1.0, length: 5.0, chamferRadius: 0.2)
+        gunStockShape.materials.first?.diffuse.contents = UIColor.brownColor()
+        let gunStockNode = SCNNode(geometry: gunStockShape)
+        gunStockNode.position.z = 10
+        gunStockNode.name = "Gun"
+        gunStockNode.pivot = SCNMatrix4MakeTranslation(0, 0, 2.5)
+        scnScene.rootNode.addChildNode(gunStockNode)
+        
+        let gunBarrelShape = SCNCylinder(radius: 0.4, height: 7.0)
+        gunBarrelShape.materials.first?.diffuse.contents = UIColor.silverColor()
+        let gunBarrelNode = SCNNode(geometry: gunBarrelShape)
+        gunBarrelNode.position.z = -4
+        gunBarrelNode.rotation.x = 90
+        gunBarrelNode.rotation.y = 0
+        gunBarrelNode.rotation.z = 0
+        gunBarrelNode.rotation.w = 29.85
+        gunStockNode.addChildNode(gunBarrelNode)
+        
+        let gunSightShape = SCNBox(width: 0.2, height: 1.0, length: 0.5, chamferRadius: 0)
+        gunSightShape.materials.first?.diffuse.contents = UIColor.silverColor()
+        let gunSightNode = SCNNode(geometry: gunSightShape)
+        gunSightNode.position.z = 0.5
+        gunSightNode.position.y = 3.1
+        gunBarrelNode.addChildNode(gunSightNode)
+        
+        return gunStockNode
+    }
+    
+    func pointGun(xTouch : CGFloat, yTouch : CGFloat) {
+        let xDir = (self.scnView.center.x - xTouch)/200
+        let yDir = (self.scnView.frame.midY - (yTouch - 50))/200
+        let direction = SCNVector3(yDir, xDir, 0)
+        gun.eulerAngles = direction
+    }
+    
+//    func fireBuckshot(xTouch : CGFloat, yTouch : CGFloat) {
+//        let xDir = -(self.scnView.center.x - xTouch)/1.5
+//        let yDir = (self.scnView.frame.midY - yTouch)/1.5
+//        let direction = SCNVector3(xDir, yDir, -300)
+//        let geometryNode1 = makeBullet()
+//        geometryNode1.physicsBody?.applyForce(direction, impulse: true)
+//        geometryNode1.position.y = 4
+//        let geometryNode2 = makeBullet()
+//        geometryNode2.physicsBody?.applyForce(direction, impulse: true)
+//        geometryNode2.position.y = 4.5
+//        let geometryNode3 = makeBullet()
+//        geometryNode3.physicsBody?.applyForce(direction, impulse: true)
+//        geometryNode3.position.y = 4
+//        geometryNode3.position.x = 0.5
+//        scnScene.rootNode.addChildNode(geometryNode1)
+//        scnScene.rootNode.addChildNode(geometryNode2)
+//        scnScene.rootNode.addChildNode(geometryNode3)
+//    }
+    
+    func makeBullet() -> SCNNode {
+        let bullet = SCNNode(geometry: SCNSphere(radius: 0.1))
+        bullet.physicsBody = SCNPhysicsBody(type: .Dynamic, shape: nil)
+        bullet.physicsBody?.affectedByGravity = false
+        bullet.geometry?.materials.first?.diffuse.contents = UIColor.blackColor()
+        bullet.position.z = 20
+        bullet.name = "bullet"
+        bullet.physicsBody?.contactTestBitMask = 1
+        return bullet
     }
     
     func cleanScene() {
         for node in scnScene.rootNode.childNodes {
-            if node.presentationNode.position.y < -10 ||
-               node.presentationNode.position.z < -100 {
+            if node.presentationNode.position.y < -50 ||
+               node.presentationNode.position.z < -200 {
                 node.removeFromParentNode()
             }
         }
@@ -143,13 +197,18 @@ class GameViewController: UIViewController {
 //        }
 //    }
 //    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let xTouch = touches.first?.locationInView(self.scnView).x
+        let yTouch = touches.first?.locationInView(self.scnView).y
+        pointGun(xTouch!, yTouch: yTouch!)
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
         let bulletQueue = dispatch_queue_create("bullet queue", DISPATCH_QUEUE_SERIAL)
-//        dispatch_async(dispatch_get_main_queue()) {
         dispatch_async(bulletQueue) {
-            let xDir = -(self.scnView.center.x - (touches.first?.locationInView(self.scnView).x)!)/3
-            let yDir = ((self.scnView.frame.maxY - (touches.first?.locationInView(self.scnView).y)!)/6)
-            self.fireGun(SCNVector3(xDir, yDir, -150))
+            let xTouch = touches.first?.locationInView(self.scnView).x
+            let yTouch = touches.first?.locationInView(self.scnView).y
+            self.fireGun(xTouch!, yTouch: yTouch!)
         }
     }
     
@@ -182,7 +241,7 @@ extension GameViewController : SCNSceneRendererDelegate {
         if time > spawnTime {
             let shapeQueue : dispatch_queue_t = dispatch_queue_create("shape queue", DISPATCH_QUEUE_SERIAL)
             dispatch_async(shapeQueue) {
-                self.spawnShape()
+                self.spawnDisks()
             }
             spawnTime = time + NSTimeInterval(Float.random(min: 1.0, max: 3.0))
             cleanScene()
